29691f963a60f78f471d0f367922080e0b2e563a
Nike Ruiz <nikedelcarmen@gmail.com>
2025-12-07 19:31:27 -0600

    Agregando archivo app

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+# Title
+st.title("Mi primera app")
+
+# Sidebar
+st.sidebar.header("Settings")
+name = st.sidebar.text_input("Ingresa tu nombre")
+# Conditional greeting
+if name:
+st.info(f"Hola, {name}! Gracias.")
